58a37b82ece2a8200bd10aafa94d2a000df2947a
Yuna-commits <ksm13563661@gmail.com>
2025-10-27 09:10:54 +0900

    component-property #1

---
+++ 03.Component-property/ex01/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./assets/css/styles.css";
+import GroceryList from "./GroceryList";
+
+function App() {
+    const groceries = [
+        { name: "Bread", count: 10 },
+        { name: "Milk", count: 20 },
+        { name: "Egg", count: 30 },
+        { name: "Banana", count: 40 },
+    ];
+
+    return (
+        <div id={"App"}>
+            <h1 className={"title"}>{"Grocery List"}</h1>
+            <GroceryList groceries={groceries} />
+        </div>
+    );
+}
+
+export default App;
