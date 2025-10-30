194497d69dfb158beda9f2b2268566a5a9606549
Yuna-commits <ksm13563661@gmail.com>
2025-10-30 09:52:27 +0900

    kanbanboard #2

--- 10.Integration/kanbanboard/frontend/src/Card.js
+++ 10.Integration/kanbanboard/frontend/src/Card.js
@@ -1,17 +1,35 @@
-import React from "react";
-import { _Card, Card_Title, Card_Title_Open } from "./assets/scss/Card.scss";
+import React, { useState } from "react";
+import {
+    _Card,
+    Card_Title,
+    Card_Title_Open,
+    Card_Details,
+} from "./assets/scss/Card.scss";
 import TaskList from "./TaskList.js";
 
 function Card({ title, description, tasks }) {
+    // task 목록 보이기(true), 안 보이기(false)
+    const [showDetails, setShowDetails] = useState(true);
+
     return (
         <div className={_Card}>
-            <div className={`${Card_Title} ${Card_Title_Open}`}>
+            <div
+                className={`${Card_Title} ${
+                    showDetails ? Card_Title_Open : ""
+                }`}
+                onClick={() => {
+                    // toggle
+                    setShowDetails(!showDetails);
+                }}
+            >
                 &nbsp;{title}
             </div>
-            <div className="Card_Details">
-                {description}
-                <TaskList tasks={tasks} />
-            </div>
+            {showDetails ? (
+                <div className={Card_Details}>
+                    {description}
+                    <TaskList tasks={tasks} />
+                </div>
+            ) : null}
         </div>
     );
 }
--- 10.Integration/kanbanboard/frontend/src/Task.js
+++ 10.Integration/kanbanboard/frontend/src/Task.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { _Task, Task_Remove } from "./assets/scss/Task.scss";
 
 function Task({ name, done }) {
+    const [doneChecked, setDoneChecked] = useState(done);
+
     return (
         <li className={_Task}>
-            <input type="checkbox" checked={done} />
+            <input
+                type="checkbox"
+                checked={doneChecked}
+                onChange={() => {
+                    setDoneChecked(!doneChecked);
+                }}
+            />
             &nbsp;{name}&nbsp;
             <a href="#" className={Task_Remove}></a>
         </li>
