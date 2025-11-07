a8f430dcbe29ccd8e98f70222b41bf29ab0978fb
Yuna-commits <ksm13563661@gmail.com>
2025-11-07 11:23:49 +0900

    config

--- 10.Integration/kanbanboard/frontend/config/webpack.config.js
+++ 10.Integration/kanbanboard/frontend/config/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
+const webpack = require("webpack");
+const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
 
 module.exports = {
     mode: "development",
     entry: path.resolve("src/index.js"),
     output: {
-        path: path.resolve("public"),
+        path: path.resolve("../backend/src/main/resources"),
         filename: "assets/js/main.js",
         assetModuleFilename: "assets/images/[hash][ext]",
     },
@@ -37,6 +39,16 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new CaseSensitivePathsPlugin(),
+        new webpack.DefinePlugin({
+            API_BASE_URL: JSON.stringify(
+                process.env.NODE_ENV === "development"
+                    ? "/api"
+                    : "http://aws.my-ec2.com/api"
+            ),
+        }),
+    ],
     devServer: {
         host: "0.0.0.0",
         port: 9090,
@@ -47,5 +59,19 @@ module.exports = {
         },
         compress: true,
         hot: false,
+        historyApiFallback: true,
+        // CORS 문제 우회, 방지(임시 해결책)
+        proxy: [
+            /**
+             * - 9090 서버가 /api 요청을 받으면 devServer가 8080으로 대신 전달
+             * - 브라우저는 같은 origin(9090)으로 인식 -> CORS 검사 x
+             *
+             * - /api, /assets(이미지 업로드) 요청을 8080으로 전달
+             */
+            {
+                context: ["/api", "/assets"],
+                target: "http://localhost:8080",
+            },
+        ],
     },
 };
